43194e401efc5db12e0ea146adba32637caf1dc5
smeghead7 <smeghead7@e9814aaf-759c-4515-a6df-38e11005d992>
2010-08-11 18:12:59 +0000

    use clojure.contrib.sql. instead of directly Java.
    git-svn-id: https://clclcl.svn.sourceforge.net/svnroot/clclcl/trunk/clclcl@32 e9814aaf-759c-4515-a6df-38e11005d992

--- src/clclcl/database.clj
+++ src/clclcl/database.clj
@@ -11,33 +11,6 @@
            :subprotocol "derby"
            :subname *database-path*})
 
-;(def *create-table-sql-of*
-;  {:clipboard-data "create table clipboard_data(id bigint not null generated always as identity constraint clipboard_data_pk primary key, data_type varchar(36), data varchar(30000))"})
-
-;(defn db-setup-params [state params]
-;  (if params
-;    (loop [ps params
-;           i 1]
-;      (if (not (empty? ps))
-;        (let []
-;          (.setObject state i (first ps))
-;          (recur (rest ps) (inc i))))))
-;  state)
-
-;(defmacro with-statement [args & body]
-;  (let [[state sql params option] args
-;        database-path *database-path*
-;        database-option (or option "")
-;        conn (gensym)
-;        result (gensym)]
-;    `(let [~conn (DriverManager/getConnection (str "jdbc:derby:" ~database-path ~database-option))
-;           ~state (db-setup-params (.prepareStatement ~conn ~sql) ~params)
-;           ~result (let [] 
-;                     ~@body)]
-;       (.close ~state)
-;       (.close ~conn)
-;       ~result)))
-
 (defn db-init []
   (.newInstance (Class/forName "org.apache.derby.jdbc.EmbeddedDriver"))
   (let [dir (File. *database-path*)]
