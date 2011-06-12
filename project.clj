7e19f407920b86f23952768184c7cdb41875596f
smeghead <smeghead7@gmail.com>
2011-06-12 20:53:43 +0900

    prepare release.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clclcl "0.2.2-beta"
+(defproject clclcl "0.2.3-beta"
             :description "access to clipboard histories."
             :dependencies [[org.clojure/clojure "1.2.0"]
                            [org.clojure/clojure-contrib "1.2.0"]
